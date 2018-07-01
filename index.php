17a5bc28a298b1b380d568f389e302ea004c1d6c
ddaaat <sadhat91@gmail.com>
2018-07-01 13:50:47 +0800

    activity 3

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>activity#3</title>
+	Name: Mohammad Shadat A. Taib
+	BSIT-4
+	<a href="sectionA.php"></a>
+	<a href="sectionB.php"></a>
+	<a href="sectionC.php"></a>
+	<a href="sectionD.php"></a>
+	<a href="sectionE.php"></a>
+	<a href="sectionF.php"></a>
+	<a href="sectionG.php"></a>
+	<a href="sectionH.php"></a>
+	<a href="sectionI.php"></a>
+</head>
+<body>
+
+</body>
+</html>
